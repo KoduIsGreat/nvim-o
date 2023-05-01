b072d3c73ca0b6ed0a7a5bca13366cf29cbb56e7
Adam Shelton <adamshelton@rti.org>
2023-05-01 14:50:26 -0400

    git log

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -32,7 +32,10 @@ autocmd("BufWinEnter", {
         vim.keymap.set("n", "<leader>P", function()
             vim.cmd.Git({'pull',  '--rebase'})
         end, opts)
-
+        
+        vim.keymap.set("n", "<leader>gl", function()
+            vim.cmd.Git('log')
+        end, opts)
         -- NOTE: It allows me to easily set the branch i am pushing and any tracking
         -- needed if i did not set the branch up correctly
         vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
