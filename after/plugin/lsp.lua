d3ee7deb7910884635d8f47db6b6b1a1129bdbdf
Adam Shelton <adamshelton@rti.org>
2023-08-19 16:27:10 -0400

    cleanup

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,65 +1,78 @@
 local lsp = require("lsp-zero")
 
-
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-  'tsserver',
-  'rust_analyzer',
-  'gopls',
-  'volar',
+	"tsserver",
+	"rust_analyzer",
+	"gopls",
+	"volar",
 })
 
 -- Fix Undefined global 'vim'
-lsp.configure('lua-language-server', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
+lsp.configure("lua-language-server", {
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
 })
 
-
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-cmp.event:on(
-  'confirm_done',
-  cmp_autopairs.on_confirm_done()
-)
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+	["<C-y>"] = cmp.mapping.confirm({ select = true }),
+	["<C-Space>"] = cmp.mapping.complete(),
 })
 
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
+cmp_mappings["<Tab>"] = nil
+cmp_mappings["<S-Tab>"] = nil
 
 lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
+	mapping = cmp_mappings,
 })
 lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
+	suggest_lsp_servers = false,
+	sign_icons = {
+		error = "E",
+		warn = "W",
+		hint = "H",
+		info = "I",
+	},
 })
 
 lsp.on_attach(function(client, bufnr)
-  require("navigator.lspclient.mapping").setup({ client = client, bufnr = bufnr }) -- setup navigator keymaps here,
-  require("navigator.dochighlight").documentHighlight(bufnr)
-  require("navigator.codeAction").code_action_prompt(bufnr)
+	require("navigator.lspclient.mapping").setup({ client = client, bufnr = bufnr }) -- setup navigator keymaps here,
+	require("navigator.dochighlight").documentHighlight(bufnr)
+	require("navigator.codeAction").code_action_prompt(bufnr)
 end)
+local lspconfig = require("lspconfig")
+lspconfig.volar.setup({
+	on_init = function(client)
+		client.server_capabilities.documentFormattingRangeProvider = false
+	end,
+})
+
+lspconfig.tsserver.setup({
+	on_init = function(client)
+		client.server_capabilities.document_formatting = false
+		client.server_capabilities.document_range_formatting = false
+	end,
+})
 
 lsp.setup()
-
 vim.diagnostic.config({
-    virtual_text = true
+	virtual_text = true,
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		vim.bo[args.buf].formatexpr = nil
+	end,
 })
