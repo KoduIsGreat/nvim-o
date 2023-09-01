abef4ccbe4c46e33b2903aefa38c39c0e34c1874
Adam Shelton <adamshelton@rti.org>
2023-09-01 11:28:03 -0400

    stuff

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,76 +3,112 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-	"tsserver",
-	"rust_analyzer",
-	"gopls",
-	"volar",
+  "tsserver",
+  "rust_analyzer",
+  "gopls",
+  "volar",
 })
 
 -- Fix Undefined global 'vim'
 lsp.configure("lua-language-server", {
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim" },
-			},
-		},
-	},
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+    },
+  },
 })
-
+-- lsp.configure("vue-language-serverr", {
+--   settings = {
+--     Vue = {
+--       format = {
+--         enable = false,
+--       },
+--     },
+--   },
+-- })
 local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-	["<C-y>"] = cmp.mapping.confirm({ select = true }),
-	["<C-Space>"] = cmp.mapping.complete(),
+  ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+  ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+  ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
 })
 
 cmp_mappings["<Tab>"] = nil
 cmp_mappings["<S-Tab>"] = nil
 
 lsp.setup_nvim_cmp({
-	mapping = cmp_mappings,
+  mapping = cmp_mappings,
 })
+-- lsp.format_on_save("gq", {
+--   format_opts = {
+--     async = false,
+--     timeout_ms = 10000,
+--   },
+--   servers = {
+--     ["volar"] = { "vue" },
+--     ["tsserver"] = { "javascript", "typescript" },
+--     ["gopls"] = { "go" },
+--     -- ["rust_analyzer"] = { "rust" },
+--     -- ['lua_ls'] = {'lua'},
+--     -- ['rust_analyzer'] = {'rust'},
+--     -- if you have a working setup with null-ls
+--     -- you can specify filetypes it can format.
+--     -- ['null-ls'] = {'javascript', 'typescript'},
+--   },
+-- })
 lsp.set_preferences({
-	suggest_lsp_servers = false,
-	sign_icons = {
-		error = "E",
-		warn = "W",
-		hint = "H",
-		info = "I",
-	},
+  suggest_lsp_servers = false,
+  sign_icons = {
+    error = "E",
+    warn = "W",
+    hint = "H",
+    info = "I",
+  },
 })
 
 lsp.on_attach(function(client, bufnr)
-	require("navigator.lspclient.mapping").setup({ client = client, bufnr = bufnr }) -- setup navigator keymaps here,
-	require("navigator.dochighlight").documentHighlight(bufnr)
-	require("navigator.codeAction").code_action_prompt(bufnr)
+  require("navigator.lspclient.mapping").setup({ client = client, bufnr = bufnr }) -- setup navigator keymaps here,
+  require("navigator.dochighlight").documentHighlight(bufnr)
+  require("navigator.codeAction").code_action_prompt(bufnr)
 end)
-local lspconfig = require("lspconfig")
-lspconfig.volar.setup({
-	on_init = function(client)
-		client.server_capabilities.documentFormattingRangeProvider = false
-	end,
+
+require("lspconfig").volar.setup({
+  on_attach = function(client)
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentFormattingRangeProvider = false
+  end,
+  on_init = function(client)
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentFormattingRangeProvider = false
+  end,
 })
 
-lspconfig.tsserver.setup({
-	on_init = function(client)
-		client.server_capabilities.document_formatting = false
-		client.server_capabilities.document_range_formatting = false
-	end,
+require("lspconfig").tsserver.setup({
+  on_attach = function(client)
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentFormattingRangeProvider = false
+  end,
+  on_init = function(client)
+    client.resolved_capabilities.document_formatting = false
+    client.resolved_capabilities.document_range_formatting = false
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentFormattingRangeProvider = false
+  end,
 })
 
 lsp.setup()
 vim.diagnostic.config({
-	virtual_text = true,
-})
-
-vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(args)
-		vim.bo[args.buf].formatexpr = nil
-	end,
+  virtual_text = true,
 })
--- lua/koduisgreat/packer.lua
+++ lua/koduisgreat/packer.lua
@@ -7,124 +7,126 @@ vim.keymap.set('n', '<leader>pcs', '<cmd>PackerSync<CR>', { noremap = true, sile
 vim.keymap.set('n', '<leader>pcu', '<cmd>PackerUpdate<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>pcc', '<cmd>PackerClean<CR>', { noremap = true, silent = true })
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
-    -- Language Specific  Plugins
-    use({
-        'ray-x/go.nvim',
-        config = function()
-            require('go').setup()
-        end
-    })
-    use 'ray-x/guihua.lua' -- recommended if need floating window support
-    -- Appearance plugins
-    use({
-        'rose-pine/neovim',
-        as = 'rose-pine',
-        config = function()
-            vim.cmd('colorscheme rose-pine')
-        end
-    })
-    use { "catppuccin/nvim", as = "catppuccin" }
-    use 'nvim-tree/nvim-web-devicons'
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-    }
-    -- Developer expereince plugins
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+  -- Language Specific  Plugins
+  use({
+    'ray-x/go.nvim',
+    config = function()
+      require('go').setup()
+    end
+  })
+  use 'ray-x/guihua.lua' -- recommended if need floating window support
+  -- Appearance plugins
+  use({
+    'rose-pine/neovim',
+    as = 'rose-pine',
+    config = function()
+      vim.cmd('colorscheme rose-pine')
+    end
+  })
+  use { "catppuccin/nvim", as = "catppuccin" }
+  use 'nvim-tree/nvim-web-devicons'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+  -- Developer expereince plugins
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
-    use {
-        "windwp/nvim-autopairs",
-        config = function() require("nvim-autopairs").setup {} end
-    }
-    use("tpope/vim-fugitive")
-    use("tpope/vim-dadbod")
-    use("tpope/vim-commentary")
-    use("kristijanhusak/vim-dadbod-completion")
-    use("kristijanhusak/vim-dadbod-ui")
-    use {
-        "folke/which-key.nvim",
-        config = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-            require("which-key").setup {
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            }
-        end
-    }
-    use {
-        'nvim-tree/nvim-tree.lua',
-        requires = {
-            'nvim-tree/nvim-web-devicons', -- optional
-        },
-        config = function()
-            require("nvim-tree").setup {}
-        end
-    }
-    use("github/copilot.vim")
-    use({
-        "folke/trouble.nvim",
-        config = function()
-            require("trouble").setup {
-                icons = false,
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            }
-        end
-    })
-    use { 'mhartington/formatter.nvim' }
-    use("theprimeagen/harpoon")
-    use("mbbill/undotree")
-    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-    use("nvim-treesitter/playground")
-    use 'nvim-treesitter/nvim-treesitter-context'
-    -- LSP plugins
-    use("williamboman/mason.nvim")
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    -- or                            , branch = '0.1.x',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+  use {
+    "windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+  }
+  use("tpope/vim-fugitive")
+  use("tpope/vim-dadbod")
+  use("tpope/vim-commentary")
+  use("kristijanhusak/vim-dadbod-completion")
+  use("kristijanhusak/vim-dadbod-ui")
+  use {
+    "folke/which-key.nvim",
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      require("which-key").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end
+  }
+  use("github/copilot.vim")
+  use({
+    "folke/trouble.nvim",
+    config = function()
+      require("trouble").setup {
+        icons = false,
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  })
+  use { 'mhartington/formatter.nvim' }
+  use("theprimeagen/harpoon")
+  use("mbbill/undotree")
+  use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+  use("nvim-treesitter/playground")
+  use 'nvim-treesitter/nvim-treesitter-context'
+  -- LSP plugins
+  use("williamboman/mason.nvim")
 
-    use({
-      "ray-x/navigator.lua",
-      requires = {
-        { "ray-x/guihua.lua", run = "cd lua/fzy && make" },
-        { "neovim/nvim-lspconfig" },
-        { "nvim-treesitter/nvim-treesitter" },
-      },
-      config = function()
-        require("navigator").setup({
-          mason = true,
-          lsp = {
-            disable_lsp = "all",
-          }
-        })
-      end,
-    })
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
-
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-nvim-lua' },
-
-            -- Snippets
-            { 'L3MON4D3/LuaSnip' },
-            { 'rafamadriz/friendly-snippets' },
+  use({
+    "ray-x/navigator.lua",
+    requires = {
+      { "ray-x/guihua.lua",               run = "cd lua/fzy && make" },
+      { "neovim/nvim-lspconfig" },
+      { "nvim-treesitter/nvim-treesitter" },
+    },
+    config = function()
+      require("navigator").setup({
+        mason = true,
+        lsp = {
+          disable_lsp = "all",
+          format_on_save = false,
+          disable_format_cap = { "volar", "tsserver" },
         }
+      })
+    end,
+  })
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    requires = {
+      -- LSP Support
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
+
+      -- Autocompletion
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-buffer' },
+      { 'hrsh7th/cmp-path' },
+      { 'saadparwaiz1/cmp_luasnip' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'hrsh7th/cmp-nvim-lua' },
+
+      -- Snippets
+      { 'L3MON4D3/LuaSnip' },
+      { 'rafamadriz/friendly-snippets' },
     }
+  }
 end)
